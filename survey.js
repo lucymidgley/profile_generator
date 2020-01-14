5b1361bf6e375c7270bd24989833c97c4e3d2174
Lucy <lucymidgley13@gmail.com>
2020-01-14 22:11:20 +0000

    Linted code and indented callbacks

--- survey.js
+++ survey.js
@@ -10,29 +10,26 @@ let hatedFood = '';
 let favFood = '';
 let song = '';
 let superPower = '';
-let paragraph = `${name} really enjoys to ${activity1} on the weeknds whilst listening to ${musicGenre}. While ${name} truly despises ${hatedFood},
-				 ${name} loves to eat ${favFood}. If ${name} was stranded on a desert island and could only listen to one song, that thing would be ${song}.
-				 In ${name}'s world, no superpower is more desirable than the ability to ${superPower}.`
 rl.question('What is your name? ', (answer) => {
-  	name = answer;
-rl.question('what do you enjoy to do on the weekends? ', (answer) => {
-	activity1 = answer;
-rl.question('What is your favorite music genre? ', (answer) => {
-	 musicGenre= answer;
-rl.question('What is a food that you absolutely hate? ', (answer) => {
-	 hatedFood = answer;
-rl.question('What is your favorite food? ', (answer) => {
-	 favFood = answer;
-rl.question('If you were stranded on an island, what one song would you have with you? ', (answer) => {
-	 song = answer;
-rl.question('If you could have any super-human ability, what would it be? ', (answer) => {
-	 superPower = answer;
-	 console.log(`\n${name} really enjoys to ${activity1} on the weekends whilst listening to ${musicGenre}. While ${name} truly despises ${hatedFood}, ${name} loves to eat ${favFood}. If ${name} was stranded on a desert island and could only listen to one song, it would be ${song}. In ${name}'s world, no superpower is more desirable than the ability to ${superPower}.`);
-  rl.close();
-});
-});
-});
-});
-});
-});
+  name = answer;
+  rl.question('what do you enjoy to do on the weekends? ', (answer) => {
+    activity1 = answer;
+    rl.question('What is your favorite music genre? ', (answer) => {
+      musicGenre = answer;
+      rl.question('What is a food that you absolutely hate? ', (answer) => {
+        hatedFood = answer;
+        rl.question('What is your favorite food? ', (answer) => {
+          favFood = answer;
+          rl.question('If you were stranded on an island, what one song would you have with you? ', (answer) => {
+            song = answer;
+            rl.question('If you could have any super-human ability, what would it be? ', (answer) => {
+              superPower = answer;
+              console.log(`\n${name} really enjoys to ${activity1} on the weekends whilst listening to ${musicGenre}. While ${name} truly despises ${hatedFood}, ${name} loves to eat ${favFood}. If ${name} was stranded on a desert island and could only listen to one song, it would be ${song}. In ${name}'s world, no superpower is more desirable than the ability to ${superPower}.`);
+              rl.close();
+            });
+          });
+        });
+      });
+    });
+  });
 });
