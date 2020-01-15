a1dda10546aff5e6686a56da64f7721022084294
Lucy <lucymidgley13@gmail.com>
2020-01-15 06:30:19 +0000

    Fixed some typos

--- survey2.js
+++ survey2.js
@@ -10,7 +10,7 @@ const askquestion = function(qArray, n, aArray) {
 
   if (n === qArray.length) {
     rl.close();
-    return console.log(`\n${aArray[0]} really enjoys to ${aArray[1]} on the weekends whilst listening to ${aArray[2]}. While ${aArray[0]} truly despises ${aArray[3]}, ${aArray[4]} loves to eat ${aArray[5]}. If ${aArray[0]} was stranded on a desert island and could only listen to one song, it would be ${aArray[6]}. In ${aArray[0]}'s world, no superpower is more desirable than the ability to ${aArray[6]}.`);
+    return console.log(`\n${aArray[0]} really enjoys to ${aArray[1]} on the weekends whilst listening to ${aArray[2]}. While ${aArray[0]} truly despises ${aArray[3]},  ${aArray[0]} loves to eat ${aArray[4]}. If ${aArray[0]} was stranded on a desert island and could only listen to one song, it would be ${aArray[5]}. In ${aArray[0]}'s world, no superpower is more desirable than the ability to ${aArray[6]}.`);
   } else {
     rl.question(qArray[n], (answer) => {
       aArray.push(answer);
